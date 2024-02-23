71f8d476384e6b817eac20d22ae19814f17d3cca
Lyle Dean <dean@lyle.dev>
2024-02-23 21:14:44 +0000

    Update Tree Sitter Lib

--- target/debug/deps/tree_sitter_llvm_mir-4832ac9fb7f113ad.d
+++
@@ -1,8 +0,0 @@
-/Users/lyledean/opensource/tree-sitter-llvm-mir/target/debug/deps/libtree_sitter_llvm_mir-4832ac9fb7f113ad.rmeta: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
-
-/Users/lyledean/opensource/tree-sitter-llvm-mir/target/debug/deps/libtree_sitter_llvm_mir-4832ac9fb7f113ad.rlib: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
-
-/Users/lyledean/opensource/tree-sitter-llvm-mir/target/debug/deps/tree_sitter_llvm_mir-4832ac9fb7f113ad.d: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
-
-bindings/rust/lib.rs:
-bindings/rust/../../src/node-types.json:
--- target/debug/deps/tree_sitter_llvm_mir-a5150797120ab9eb.d
+++
@@ -1,6 +0,0 @@
-/Users/lyledean/opensource/tree-sitter-llvm-mir/target/debug/deps/libtree_sitter_llvm_mir-a5150797120ab9eb.rmeta: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
-
-/Users/lyledean/opensource/tree-sitter-llvm-mir/target/debug/deps/tree_sitter_llvm_mir-a5150797120ab9eb.d: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
-
-bindings/rust/lib.rs:
-bindings/rust/../../src/node-types.json:
